53306e191997bcd443f7681e7e2d6d1377f73f31
yoshimasa36g <ashihana785@gmail.com>
2019-12-23 23:42:56 +0900

    wip

--- keyboards/crkbd/keymaps/custom/keymap.c
+++ keyboards/crkbd/keymaps/custom/keymap.c
@@ -34,13 +34,13 @@ enum macro_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------.                ,-----------------------------------------.
-     KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,KC_BSPC,\
+    KC_GRV,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,KC_BSPC,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L,KC_SCLN,KC_QUOT,\
+    KC_TAB,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L,KC_SCLN,KC_QUOT,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-     KC_ESC,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_RSFT,\
+    KC_ESC,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_BSLS,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                        KC_LGUI, LOWER, LSFT_T(KC_SPC), KC_ENT, RAISE,KC_RALT \
+                        KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1),KC_RALT \
                               //`--------------------'  `--------------------'
   ),
 
