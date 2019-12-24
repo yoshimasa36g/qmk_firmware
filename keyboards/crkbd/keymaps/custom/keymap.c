58adbe3da09c11f06ef9cd4cc124d1ee2dd83fdb
yoshimasa36g <ashihana785@gmail.com>
2019-12-24 22:37:18 +0900

    crkbd maping

--- keyboards/crkbd/keymaps/custom/keymap.c
+++ keyboards/crkbd/keymaps/custom/keymap.c
@@ -38,33 +38,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
     KC_TAB,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                   KC_H,  KC_J,  KC_K,  KC_L,KC_SCLN,KC_QUOT,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_ESC,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                   KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_BSLS,\
+    KC_ESC,  KC_Z,  KC_X,  KC_C,  KC_V,  LT(_RAISE, KC_B), LT(_LOWER, KC_N),  KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_BSLS,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                        KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1),KC_RALT \
+            KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1),KC_RALT \
                               //`--------------------'  `--------------------'
   ),
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------.                ,-----------------------------------------.
-     KC_ESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                   KC_6,  KC_7,  KC_8,  KC_9,  KC_0,KC_BSPC,\
+    KC_GRV,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_NO,KC_NO,\
+    KC_TAB,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                   KC_6,  KC_7,  KC_8,  KC_9,  KC_0,KC_NO,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,\
+    KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_UNDS,KC_MINS,KC_EQL, KC_PLUS,KC_LBRC,KC_RBRC,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT \
+            KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1),KC_RALT \
                               //`--------------------'  `--------------------'
   ),
 
   [_RAISE] = LAYOUT( \
   //,-----------------------------------------.                ,-----------------------------------------.
-     KC_ESC,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,              KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_BSPC,\
+    KC_GRV,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,              KC_MINS,KC_EQL,KC_PLUS,KC_LCBR,KC_RCBR,KC_DEL,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_MINS,KC_EQL,KC_LCBR,KC_RCBR,KC_PIPE,KC_GRV,\
+    KC_TAB, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                KC_LEFT,KC_DOWN,KC_UP, KC_RIGHT,KC_NO, KC_NO,\
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
-    KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_UNDS,KC_PLUS,KC_LBRC,KC_RBRC,KC_BSLS,KC_TILD,\
+    KC_ESC, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO,                KC_UNDS,KC_MINS,KC_EQL,KC_PLUS,KC_LBRC,KC_RBRC,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                KC_LGUI, LOWER,KC_SPC,   KC_ENT, RAISE,KC_RALT \
+            KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1),KC_RALT \
                               //`--------------------'  `--------------------'
   ),
 
