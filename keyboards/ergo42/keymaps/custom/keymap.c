b10c76b80c27e9187c61080034d32d3656577a77
yoshimasa36g <ashihana785@gmail.com>
2020-01-09 13:03:12 +0900

    modify ergo42 keymap to same as crkbd

--- keyboards/ergo42/keymaps/custom/keymap.c
+++ keyboards/ergo42/keymaps/custom/keymap.c
@@ -11,56 +11,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* BASE
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   `  |   Q  |   W  |   E  |   R  |  T   |   (  |   |   )  |  Y   |   U  |   I  |   O  |   P  | Bksp |
+   * |   `  |   Q  |   W  |   E  |   R  |  T   |      |   |      |  Y   |   U  |   I  |   O  |   P  | Bksp |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | TAB  |   A  |   S  |   D  |   F  |  G   |   ^  |   |   $  |  H   |   J  |   K  |   L  |   ;  |   '  |
+   * | TAB  |   A  |   S  |   D  |   F  |  G   |      |   |      |  H   |   J  |   K  |   L  |   ;  |   '  |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * | LSift|   Z  |   X  |   C  |   V  |  B   | SYMB |   | SYMB |  N   |   M  |   ,  |   .  |   /  |   \  |
+   * | ESC  |   Z  |   X  |   C  |   V  |B,SYMB|      |   |      |N,META|   M  |   ,  |   .  |   /  |   \  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | ESC  | LCtrl| LAlt | LGUI |CLang2|SSpace| META |   | META |SEnter|CLang1|   -  |   =  |   [  |   ]  |
+   * |      |      |      |      | LGUI |CLang2|SSpace|   |SEnter|CLang1| RALT |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [BASE] = LAYOUT( \
-    KC_GRV,   KC_Q,    KC_W,    KC_E,    KC_R,             KC_T,           S(KC_9),  S(KC_0),  KC_Y,           KC_U,             KC_I,    KC_O,   KC_P,    KC_BSPC,     \
-    KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,             KC_G,           S(KC_6),  S(KC_4),  KC_H,           KC_J,             KC_K,    KC_L,   KC_SCLN, KC_QUOT,  \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,           MO(SYMB), MO(SYMB), KC_N,           KC_M,             KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, \
-    KC_ESC,   KC_LCTL, KC_LALT, KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), MO(META), MO(META), RSFT_T(KC_ENT), RCTL_T(KC_LANG1), KC_MINS, KC_EQL, KC_LBRC, KC_RBRC \
+    KC_GRV,  KC_Q,  KC_W,  KC_E,  KC_R,    KC_T,             KC_NO,          KC_NO,          KC_Y,             KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC, \
+    KC_TAB,  KC_A,  KC_S,  KC_D,  KC_F,    KC_G,             KC_NO,          KC_NO,          KC_H,             KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+    KC_ESC,  KC_Z,  KC_X,  KC_C,  KC_V,    LT(SYMB, KC_B),   KC_NO,          KC_NO,          LT(META, KC_N),   KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, \
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1), KC_RALT, KC_NO,   KC_NO,  KC_NO,   KC_NO \
   ),
 
   /* META
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   `  |   !  |   @  |   #  |   $  |  %   |   [  |   |   ]  |  ^   |   &  |   *  |   (  |   )  | Del  |
+   * |   `  |   !  |   @  |   #  |   $  |  %   |      |   |      |  ^   |   &  |   *  |   (  |   )  | Del  |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | TAB  |   1  |   2  |   3  |   4  |  5   |   {  |   |   }  |  6   |   7  |   8  |   9  |   0  |   '  |
+   * | TAB  |   1  |   2  |   3  |   4  |  5   |      |   |      |  6   |   7  |   8  |   9  |   0  |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * | LSift|  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   |  F7  |  F8  |  F9  | F10  | F11  | F12  |   \  |
+   * | ESC  |      |      |      |      |      |      |   |      |  _   |   -  |   =  |   +  |   [  |   ]  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | ESC  | LCtrl| LAlt | LGUI |CLang2|SSpace| META |   | META |SEnter|CLang1|   -  |   =  |   [  |   ]  |
+   * |      |      |      |      | LGUI |CLang2|SSpace|   |SEnter|CLang1| RALT |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_DEL, \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    S(KC_LBRC), S(KC_RBRC), KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,      KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______\
+    KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR,  KC_PERC,          KC_NO,          KC_NO,          KC_CIRC,          KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,\
+    KC_TAB, KC_1,    KC_2,  KC_3,    KC_4,    KC_5,             KC_NO,          KC_NO,          KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,\
+    KC_ESC, KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,            KC_NO,          KC_NO,          KC_UNDS,          KC_MINS, KC_EQL,  KC_PLUS, KC_LBRC, KC_RBRC,\
+    KC_NO,  KC_NO,   KC_NO, KC_NO,   KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1), KC_RALT, KC_NO,   KC_NO,   KC_NO,   KC_NO \
   ),
 
   /* SYMB
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   `  |   Q  |   W  |   E  |   R  |  T   |   (  |   |   )  |  Y   |   U  |   I  |   O  |   P  | Del  |
+   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |      |   |      |  -   |   =  |   +  |   {  |   }  | Del  |
    * |------+------+------+------+------+------+------+   |-------------+------+------+------+------+------|
-   * | TAB  |   A  |   S  |   D  |   F  |  G   | HOME |   | END  | Left | Down |  Up  |Right |   ;  |   '  |
+   * | TAB  |  F6  |  F7  |  F8  |  F9  |  F10 |      |   |      | Left | Down |  Up  |Right |      |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * | LSift|   Z  |   X  |   C  |   V  |  B   | SYMB |   | SYMB |  N   |   M  |   ,  |   .  |   /  |   \  |
+   * | ESC  |  F11 |  F12 |      |      |      |      |   |      |  _   |   -  |   =  |   +  |   [  |   ]  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | ESC  | LCtrl| LAlt | LGUI |CLang2|SSpace| META |   | META |SEnter|CLang1|   -  |   =  |   [  |   ]  |
+   * |      |      |      |      | LGUI |CLang2|SSpace|   |SEnter|CLang1| RALT |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
-    _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+    KC_GRV, KC_F1,  KC_F2,  KC_F3, KC_F4,   KC_F5,            KC_NO,          KC_NO,          KC_MINS,          KC_EQL,  KC_PLUS, KC_LCBR,  KC_RCBR, KC_DEL,\
+    KC_TAB, KC_F6,  KC_F7,  KC_F8, KC_F9,   KC_F10,           KC_NO,          KC_NO,          KC_LEFT,          KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,   KC_NO,\
+    KC_ESC, KC_F11, KC_F12, KC_NO, KC_NO,   KC_NO,            KC_NO,          KC_NO,          KC_UNDS,          KC_MINS, KC_EQL,  KC_PLUS,  KC_LBRC, KC_RBRC,\
+    KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_LGUI, LCTL_T(KC_LANG2), LSFT_T(KC_SPC), RSFT_T(KC_ENT), RCTL_T(KC_LANG1), KC_RALT, KC_NO,   KC_NO,    KC_NO,   KC_NO \
   )
 
 };
